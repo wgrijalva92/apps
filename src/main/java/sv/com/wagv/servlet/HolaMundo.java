42d98c29fe1b1b2ace554522157236da4ce684d7
wilmer grijalva <wgdesarrollo@gmail.com>
2022-05-16 16:15:41 -0600

    probando subir codigo a repositorio

---
+++ src/main/java/sv/com/wagv/servlet/HolaMundo.java
@@ -0,0 +1,28 @@
+package sv.com.wagv.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/holaMundo")
+public class HolaMundo extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Hola mundo desde un servlet");
+    }
+    
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
